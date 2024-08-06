65e67f7fe539f0e67c02725e02e7349ba1342297
Olga Tas <olgatas.f@gmail.com>
2024-08-06 16:07:49 +0300

    init

---
+++ adaptive-menu.js
@@ -0,0 +1,9 @@
+document.getElementById('burger').onclick = function () {
+    document.getElementById('menu').classList.add('open');
+}
+
+document.querySelectorAll('#menu *').forEach((item) => {
+    item.onclick = () => {
+        document.getElementById('menu').classList.remove('open');
+    }
+})
