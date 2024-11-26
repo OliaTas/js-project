8fc532014e21c181cd91f135a0930a37f1ddbf68
Olga Tas <olgatas.f@gmail.com>
2024-11-26 15:34:36 +0000

    added changes to income.js

--- frontend/src/components/incomes.js
+++ frontend/src/components/incomes.js
@@ -1,62 +1,88 @@
+import {CustomHttp} from "../services/custom-http.js";
+import config from "../../config/config.js";
+import {Auth} from "../services/auth.js";
+
+
+
 export class Incomes {
     constructor() {
-        this.editButton = document.getElementsByClassName('btn-edit');
-        this.cardName = document.getElementById('card-text');
-        this.deleteButton = document.getElementsByClassName('btn-delete');
-        this.modal = document.getElementById('modal');
-        this.btnDelete = document.getElementById('btn-y-delete');
-        this.btnNoDelete = document.getElementById('btn-no-delete');
-        this.addCardBtn = document.getElementById('add-btn');
-        this.block = document.getElementById('block')
-        this.cardText = document.querySelector('.card-text')
+        // this.editButton = document.getElementsByClassName('btn-edit');
+        // this.cardName = document.getElementById('card-text');
+        // this.deleteButton = document.getElementsByClassName('btn-delete');
+        // this.modal = document.getElementById('modal');
+        // this.btnDelete = document.getElementById('btn-y-delete');
+        // this.btnNoDelete = document.getElementById('btn-no-delete');
+        // this.addCardBtn = document.getElementById('add-btn');
+        // this.block = document.getElementById('block')
+        // this.cardText = document.querySelector('.card-text')
+        this.incomeCategory = null;
 
-        const that = this;
-        for (let i = 0; i < this.editButton.length; i++) this.editButton[i].onclick =  function () {
-            that.editIncomeProcess ();
-        }
+        // const that = this;
+        // for (let i = 0; i < this.editButton.length; i++) this.editButton[i].onclick = function () {
+        //     that.editIncomeProcess();
+        // }
+        //
+        // for (let i = 0; i < this.deleteButton.length; i++) this.deleteButton[i].onclick = function () {
+        //     that.deleteIncomeProcess();
+        // }
+        //
+        // this.btnDelete.onclick = function () {
+        //     that.deleteModalProcess();
+        // }
+        //
+        // this.btnNoDelete.onclick = function () {
+        //     that.modalProcess();
+        // }
+        //
+        // this.addCardBtn.onclick = function () {
+        //     that.addCardProcess();
+        // }
 
-        for (let i = 0; i < this.deleteButton.length; i++) this.deleteButton[i].onclick =  function () {
-            that.deleteIncomeProcess ();
-        }
+        this.init();
+    }
 
-        this.btnDelete.onclick = function () {
-            that.deleteModalProcess ();
-        }
+    async init() {
 
-        this.btnNoDelete.onclick = function () {
-            that.modalProcess ();
-        }
 
-        this.addCardBtn.onclick = function () {
-            that.addCardProcess();
-        }
+             const result = await CustomHttp.request(config.host + '/categories/income/')
+             console.log(result)
+             if (result) {
+                 if (result.error || !result.tokens.accessToken || !result.tokens.refreshToken) {
+                     throw new Error(result.message);
+                 }
+
+                 this.incomeCategory = result;
+                 console.log(this.incomeCategory.categories)
+             }
+
+
 
 
     }
 
-    editIncomeProcess () {
-        localStorage.setItem('cardText', this.cardText.value)
-        console.log(localStorage.cardText)
-        location.href = '#/edit-income';
-    }
-
-    deleteIncomeProcess () {
-       this.modal.style.display = 'flex';
-    }
-
-    deleteModalProcess () {
-
-        document.querySelector('.col').remove(); //удаление карточки
-        this.modal.style.display = 'none';
-    }
-
-    modalProcess () {
-        this.modal.style.display = 'none';
-    }
-
-    addCardProcess () {
-        location.href = '#/create-income';
-    }
+    // editIncomeProcess () {
+    //     localStorage.setItem('cardText', this.cardText.value)
+    //     console.log(localStorage.cardText)
+    //     location.href = '#/edit-income';
+    // }
+    //
+    // deleteIncomeProcess () {
+    //    this.modal.style.display = 'flex';
+    // }
+    //
+    // deleteModalProcess () {
+    //
+    //     document.querySelector('.col').remove(); //удаление карточки
+    //     this.modal.style.display = 'none';
+    // }
+    //
+    // modalProcess () {
+    //     this.modal.style.display = 'none';
+    // }
+    //
+    // addCardProcess () {
+    //     location.href = '#/create-income';
+    // }
 
 
 }
---
+++ frontend/src/utils/url-manager.js
@@ -0,0 +1,15 @@
+export class UrlManager {
+    static getQueryParams() {
+        const qs = document.location.hash.split('+').join(' ');
+
+        let params = {},
+            tokens,
+            re = /[?&]([^=]+)=([^&]*)/g;
+
+        while (tokens = re.exec(qs)) {
+            params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+        }
+
+        return params;
+    }
+}
